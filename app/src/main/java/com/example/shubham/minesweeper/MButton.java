c96a6fb72bc51e69bf07ebdc7ce171676bba1ee9
sgoyal12 <shu12.sg@gmail.com>
2018-06-21 15:18:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shubham/minesweeper/MButton.java
@@ -0,0 +1,122 @@
+package com.example.shubham.minesweeper;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.widget.Button;
+
+/**
+ * Created by shubham on 6/18/2018.
+ */
+
+public class MButton extends android.support.v7.widget.AppCompatButton {
+    private int mines=0;
+    private int x,y;
+    public boolean revealStatus=false,flag=false;
+
+private int mineStatus=MainActivity.Mine_Not_Set;
+
+
+    public MButton(Context context) {
+        super(context);
+    }
+
+    public void setXY(int x,int y) {
+        this.x = x;
+        this.y=y;
+    }
+
+
+
+    public void setMines(MButton board[][], int nrow, int ncol)
+    {
+        mineStatus=MainActivity.Mine_Set;
+        int  i,j;
+        for(i=this.x-1;i<this.x+2;i++)
+        {
+            for (j=this.y-1;j<this.y+2;j++)
+            {
+                if(i>=0&&i<nrow&&j>=0&&j<ncol&&board[i][j].mineStatus==MainActivity.Mine_Not_Set)
+                    board[i][j].mines++;
+
+            }
+        }
+
+    }
+    public int getMineStatus()
+    {
+        return this.mineStatus;
+    }
+    private int getMines()
+    {
+        if(this.mineStatus==MainActivity.Mine_Not_Set)
+            return this.mines;
+        else
+            return this.mineStatus;
+    }
+    public void reveal(MButton board[][],int nrow,int ncol)
+    {
+        int m=this.getMines();
+        if(!revealStatus&&!flag) {
+            if (m != MainActivity.Mine_Set && mines == 0) {
+                this.setEnabled(false);
+                revealStatus=true;
+                revealAround(board, nrow, ncol, this.x, this.y);
+                this.setBackgroundResource(R.color.buttonDis);
+            } else if (m != MainActivity.Mine_Set && mines != 0) {
+                this.setText("" + m);
+                revealStatus=true;
+                this.setEnabled(false);
+
+            } else if(MainActivity.Mine_Set==this.mineStatus) {
+                this.setBackgroundResource(R.drawable.mines1);
+                revealStatus=true;
+                MainActivity.currentStatus=MainActivity.PLAYER_LOST;
+                revealMines(board,nrow,ncol);
+            }
+        }
+    }
+
+    public void revealAround(MButton[][] board, int nrow, int ncol, int x, int y) {
+        int i,j;
+        for(i=x-1;i<x+2;i++)
+        {
+            for (j=y-1;j<y+2;j++)
+            {
+                if(i>=0&&i<nrow&&j>=0&&j<ncol&&board[i][j].mineStatus==MainActivity.Mine_Not_Set&&!(i==x&&j==y)){
+                    int m=board[i][j].getMines();
+                    if(m!=MainActivity.Mine_Set&&mines==0) {
+                        board[i][j].setEnabled(false);
+                        revealStatus=true;
+                        board[i][j].reveal(board,nrow,ncol);
+                    }
+                    else if(m!=MainActivity.Mine_Set&& mines!=0)
+                    {
+                        board[i][j].setText(""+m);
+                        revealStatus=true;
+                        board[i][j].setEnabled(false);
+                    }
+                }
+
+
+            }
+        }
+    }
+
+    public void revealMines(MButton board[][],int nrow,int ncol)
+    {
+        for(int i=0;i<nrow;i++)
+        {
+            for(int j=0;j<ncol;j++)
+            {
+                if(board[i][j].mineStatus==MainActivity.Mine_Set)
+                board[i][j].reveal(board,nrow,ncol);
+            }
+        }
+    }
+
+
+
+    public boolean notMine() {
+        return this.mineStatus==MainActivity.Mine_Not_Set;
+    }
+}
---
+++ app/src/main/java/com/example/shubham/minesweeper/MainActivity.java
@@ -0,0 +1,204 @@
+package com.example.shubham.minesweeper;
+
+import android.graphics.drawable.GradientDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
+
+    LinearLayout rootLayout;
+    int nrow=10,ncol=10;
+    ArrayList<LinearLayout> rows;
+    public MButton[][] board;
+    public static final int Mine_Set=-1,Mine_Not_Set=-2;
+    public static final int INCOMPLETE=1,PLAYER_WON=2,PLAYER_LOST=3;
+
+     static int currentStatus;
+     static int diff=10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rootLayout=findViewById(R.id.RootLayout);
+        setBoard();
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.RESET)
+            setBoard();
+        else if(item.getItemId()==R.id.X6)
+        {
+            nrow=6;
+            ncol=6;
+            item.setChecked(true);
+            setBoard();
+        }
+        else if (item.getItemId()==R.id.X8)
+        {
+            nrow=8;
+            ncol=8;
+            item.setChecked(true);
+            setBoard();
+        }
+        else if (item.getItemId()==R.id.X10)
+        {
+            nrow=10;
+            ncol=10;
+            item.setChecked(true);
+            setBoard();
+        }
+
+        else if(item.getItemId()==R.id.easy)
+        {
+            diff=10;
+            item.setChecked(true);
+            setBoard();
+
+        }
+        else if (item.getItemId()==R.id.medium)
+        {
+            diff=6;
+            item.setChecked(true);
+            setBoard();
+        }
+        else if (item.getItemId()==R.id.high)
+        {
+            diff=4;
+            item.setChecked(true);
+            setBoard();
+        }
+        return true;
+    }
+
+    public void setBoard()
+    {   board=new MButton[nrow][ncol];
+        rows=new ArrayList<>();
+        currentStatus=INCOMPLETE;
+        rootLayout.removeAllViews();
+        for(int i=0;i<nrow;i++)
+        {
+            LinearLayout linearLayout=new LinearLayout(this);
+            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+            LinearLayout.LayoutParams layoutParams=new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,0,1);
+            linearLayout.setLayoutParams(layoutParams);
+            rootLayout.addView(linearLayout);
+            rows.add(linearLayout);
+        }
+        for(int i=0;i<nrow;i++)
+        {
+            for (int j=0;j<ncol;j++)
+            {
+                MButton button=new MButton(this);
+                LinearLayout.LayoutParams layoutParams=new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT,1);
+                button.setLayoutParams(layoutParams);
+               button.setOnLongClickListener(this);
+                button.setOnClickListener(this);
+
+                button.setTextColor(getResources().getColor(R.color.textColor));
+                GradientDrawable drawable=new GradientDrawable();
+                drawable.setShape(GradientDrawable.RECTANGLE);
+                drawable.setColor(getResources().getColor(R.color.button));
+                drawable.setCornerRadius(25);
+                drawable.setStroke(2,getResources().getColor(R.color.textColor));
+                button.setBackgroundDrawable(drawable);
+                LinearLayout row=rows.get(i);
+                row.addView(button);
+                board[i][j]=button;
+                board[i][j].setXY(i,j);
+            }
+        }
+        Random random=new Random(),random1=new Random();
+        for(int i=0;i<(ncol*nrow)/diff;)
+        {
+            int x,y;
+            x=random.nextInt(nrow);
+            y=random1.nextInt(ncol);
+            if(board[x][y].getMineStatus()!=Mine_Set)
+            {
+                board[x][y].setMines(board,nrow,ncol);
+                i++;
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(currentStatus==INCOMPLETE) {
+            MButton button = (MButton) v;
+            button.reveal(board, nrow, ncol);
+            updateStatus();
+            if(currentStatus==PLAYER_LOST)
+            {
+                Toast.makeText(MainActivity.this,"You Lost",Toast.LENGTH_SHORT).show();
+            }
+            else if(currentStatus==PLAYER_WON){
+                Toast.makeText(MainActivity.this,"You Won",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    public void updateStatus() {
+        boolean abc=true;
+        for(int i=0;i<nrow;i++)
+        {
+            for(int j=0;j<ncol;j++)
+            {
+                if(!board[i][j].revealStatus&&board[i][j].notMine())
+                {
+                    abc=false;
+                }
+
+            }
+            if(abc) {
+                currentStatus = PLAYER_WON;
+            }
+        }
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        MButton button=(MButton)v;
+       if(currentStatus==INCOMPLETE) {
+           if (!button.flag) {
+               button.flag = true;
+               button.setBackgroundResource(R.drawable.flags);
+
+           } else {
+               button.flag = false;
+               button.setBackgroundResource(R.color.button);
+               GradientDrawable drawable = new GradientDrawable();
+               drawable.setShape(GradientDrawable.RECTANGLE);
+               drawable.setCornerRadius(25);
+
+               drawable.setColor(getResources().getColor(R.color.button));
+               drawable.setStroke(2, getResources().getColor(R.color.textColor));
+               button.setBackgroundDrawable(drawable);
+           }
+
+
+       }
+        return true;
+    }
+}
+
